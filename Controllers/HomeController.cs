6e0b9d610de823ba2cc413634435e0d678e64ad3
Santiago Javier <64206519+santiagoVL03@users.noreply.github.com>
2025-01-02 12:15:21 -0500

    redirections

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,46 +43,46 @@ public class HomeController : Controller
     }
 
     // Acción para verificar si el DNI existe
-        [HttpPost]
-        public IActionResult VerificarUsuario(string dni)
+    [HttpPost]
+    public IActionResult VerificarUsuario(string dni)
+    {
+        if (!dni.All(char.IsDigit))
         {
-            if (!dni.All(char.IsDigit))
-            {
-                TempData["MessageError"] = "Ingrese Solo Numeros en el DNI";
-                return RedirectToAction("Index", "Home");
-            }
-            dni = dni.Trim();
-            var usuario = _context.usuarios.FirstOrDefault(u => u.Dni.Trim() == dni);
-
-            if (usuario == null)
-            {
-                TempData["MessageError"] = "El Usuario a Buscar no se encuentra Registrado comunicarse con su Supervisor.";
-                return RedirectToAction("Index", "Home");
-            }
-
-            if (usuario.Estado == "INACTIVO")
-            {
-                TempData["MessageError"] = "El Usuario se fue marcado como Inactivo, Comunicarse con su Supervisor.";
-                return RedirectToAction("Index", "Home");
-            }
-            
-            if (string.IsNullOrEmpty(usuario.Rol))
-            {
-                TempData["MessageError"] = "El rol del usuario no está definido. Comuníquese con su Supervisor.";
-                return RedirectToAction("Index", "Home");
-            }
-            if (usuario.Rol == "VENDEDOR")
-            {
-                HttpContext.Session.SetInt32("UsuarioId", usuario.IdUsuario);
-                return RedirectToAction("Ventas","User");
-            }
-            if (usuario.Rol == "SUPERVISOR")
-            {
-                HttpContext.Session.SetInt32("UsuarioId", usuario.IdUsuario);
-                return RedirectToAction("VistaMainSupervisor", "Supervisor");
-            }
-            TempData["MessageError"] = "Algo salio Mal en la Autenticacion";
+            TempData["MessageError"] = "Ingrese Solo Numeros en el DNI";
             return RedirectToAction("Index", "Home");
         }
+        dni = dni.Trim();
+        var usuario = _context.usuarios.FirstOrDefault(u => u.Dni.Trim() == dni);
+
+        if (usuario == null)
+        {
+            TempData["MessageError"] = "El Usuario a Buscar no se encuentra Registrado comunicarse con su Supervisor.";
+            return RedirectToAction("Index", "Home");
+        }
+
+        if (usuario.Estado == "INACTIVO")
+        {
+            TempData["MessageError"] = "El Usuario se fue marcado como Inactivo, Comunicarse con su Supervisor.";
+            return RedirectToAction("Index", "Home");
+        }
+
+        if (string.IsNullOrEmpty(usuario.Rol))
+        {
+            TempData["MessageError"] = "El rol del usuario no está definido. Comuníquese con su Supervisor.";
+            return RedirectToAction("Index", "Home");
+        }
+        if (usuario.Rol == "VENDEDOR")
+        {
+            HttpContext.Session.SetInt32("UsuarioId", usuario.IdUsuario);
+            return RedirectToAction("Ventas", "User");
+        }
+        if (usuario.Rol == "SUPERVISOR")
+        {
+            HttpContext.Session.SetInt32("UsuarioId", usuario.IdUsuario);
+            return RedirectToAction("VistaMainSupervisor", "Supervisor");
+        }
+        TempData["MessageError"] = "Algo salio Mal en la Autenticacion";
+        return RedirectToAction("Index", "Home");
+    }
 
 }
