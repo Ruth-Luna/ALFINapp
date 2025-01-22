15d81de8de4388bb0df03b353f3dde709d307437
WyADesarrolladorK <grupowyadesarrollo02@hotmail.com>
2025-01-22 15:53:21 -0500

    feat:
    Proyecto:
    + Agrega nueva archivo para la vista Perfil.
    + Agrega nueva archivo para el controlador para el perfil.
    + Agrega nuevo archivo para la vista del Login (_LayoutLogin.cshtml), contruyendo su HTML.
    + Agregamos nuevo estilo hover para cada enlace.
    + Definimos la ruta /perfil para la vista perfil.

---
+++ Controllers/PerfilController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ALFINapp.Controllers
+{
+    public class PerfilController : Controller
+    {
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
