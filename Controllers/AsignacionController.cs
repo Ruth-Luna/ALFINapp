51e4ae5794c54934caa3772f16da0e714bcd75c9
Santiago Javier <64206519+santiagoVL03@users.noreply.github.com>
2025-01-30 11:33:53 -0500

    feat: enhance AsignacionController and DTO to include additional data retrieval and improve error handling in the supervisor assignment view

--- Controllers/AsignacionController.cs
+++ Controllers/AsignacionController.cs
@@ -101,10 +101,80 @@ namespace ALFINapp.Controllers
                     return RedirectToAction("Inicio", "Administrador");
                 }
 
+                var GetUClienteEstado = await _dbServicesGeneral.GetUClienteEstado();
+                if (GetUClienteEstado.IsSuccess == false || GetUClienteEstado.data == null)
+                {
+                    TempData["MessageError"] = GetUClienteEstado.Message;
+                    return RedirectToAction("Inicio", "Administrador");
+                }
+
                 var GetUColor = await _dbServicesGeneral.GetUColor();
                 if (GetUColor.IsSuccess == false || GetUColor.data == null)
                 {
                     TempData["MessageError"] = GetUColor.Message;
+                    return RedirectToAction("Inicio", "Administrador");
+                }
+
+                var GetUColorFinal = await _dbServicesGeneral.GetUColorFinal();
+                if (GetUColorFinal.IsSuccess == false || GetUColorFinal.data == null)
+                {
+                    TempData["MessageError"] = GetUColorFinal.Message;
+                    return RedirectToAction("Inicio", "Administrador");
+                }
+
+                var GetUFrescura = await _dbServicesGeneral.GetUFrescura();
+                if (GetUFrescura.IsSuccess == false || GetUFrescura.data == null)
+                {
+                    TempData["MessageError"] = GetUFrescura.Message;
+                    return RedirectToAction("Inicio", "Administrador");
+                }
+
+                var GetUGrupoMonto = await _dbServicesGeneral.GetUGrupoMonto();
+                if (GetUGrupoMonto.IsSuccess == false || GetUGrupoMonto.data == null)
+                {
+                    TempData["MessageError"] = GetUGrupoMonto.Message;
+                    return RedirectToAction("Inicio", "Administrador");
+                }
+
+                var GetUGrupoTasa = await _dbServicesGeneral.GetUGrupoTasa();
+                if (GetUGrupoTasa.IsSuccess == false || GetUGrupoTasa.data == null)
+                {
+                    TempData["MessageError"] = GetUGrupoTasa.Message;
+                    return RedirectToAction("Inicio", "Administrador");
+                }
+
+                var GetUPropension = await _dbServicesGeneral.GetUPropension();
+                if (GetUPropension.IsSuccess == false || GetUPropension.data == null)
+                {
+                    TempData["MessageError"] = GetUPropension.Message;
+                    return RedirectToAction("Inicio", "Administrador");
+                }
+
+                var GetURangoEdad = await _dbServicesGeneral.GetURangoEdad();
+                if (GetURangoEdad.IsSuccess == false || GetURangoEdad.data == null)
+                {
+                    TempData["MessageError"] = GetURangoEdad.Message;
+                    return RedirectToAction("Inicio", "Administrador");
+                }
+
+                var GetURangoOferta = await _dbServicesGeneral.GetURangoOferta();
+                if (GetURangoOferta.IsSuccess == false || GetURangoOferta.data == null)
+                {
+                    TempData["MessageError"] = GetURangoOferta.Message;
+                    return RedirectToAction("Inicio", "Administrador");
+                }
+
+                var GetURangoTasas = await _dbServicesGeneral.GetURangoTasas();
+                if (GetURangoTasas.IsSuccess == false || GetURangoTasas.data == null)
+                {
+                    TempData["MessageError"] = GetURangoTasas.Message;
+                    return RedirectToAction("Inicio", "Administrador");
+                }
+
+                var GetUTipoCliente = await _dbServicesGeneral.GetUTipoCliente();
+                if (GetUTipoCliente.IsSuccess == false || GetUTipoCliente.data == null)
+                {
+                    TempData["MessageError"] = GetUTipoCliente.Message;
                     return RedirectToAction("Inicio", "Administrador");
                 }
 
@@ -125,8 +195,19 @@ namespace ALFINapp.Controllers
                 var GetDataLabels = new AsignacionSupervisoresDTO
                 {
                     UCampanas = GetUCampanas.data,
+                    UClienteEstado = GetUClienteEstado.data,
+                    UColor = GetUColor.data,
+                    UColorFinal = GetUColorFinal.data,
+                    UFrescura = GetUFrescura.data,
+                    UGrupoMonto = GetUGrupoMonto.data,
+                    UGrupoTasa = GetUGrupoTasa.data,
+                    UPropension = GetUPropension.data,
+                    URangoEdad = GetURangoEdad.data,
+                    URangoOferta = GetURangoOferta.data,
+                    URangoTasas = GetURangoTasas.data,
+                    UTipoCliente = GetUTipoCliente.data,
                     UUsuario = GetUUsuario.data,
-                    UTipoBase = GetUTipoBase.data
+                    UTipoBase = GetUTipoBase.data,
                 };
 
                 /*if (GetSupervisores.IsSuccess == false)
--- Models/AsignacionSupervisoresDTO.cs
+++ Models/AsignacionSupervisoresDTO.cs
@@ -8,6 +8,17 @@ namespace ALFINapp.Models
     public class AsignacionSupervisoresDTO
     {
         public List<StringDTO>? UCampanas { get; set; }
+        public List<StringDTO>? UClienteEstado { get; set; }
+        public List<StringDTO>? UColor { get; set; }
+        public List<StringDTO>? UColorFinal { get; set; }
+        public List<StringDTO>? UFrescura { get; set; }
+        public List<StringDTO>? UGrupoMonto { get; set; }
+        public List<StringDTO>? UGrupoTasa { get; set; }
+        public List<NumerosEnterosDTO>? UPropension { get; set; }
+        public List<StringDTO>? URangoEdad { get; set; }
+        public List<StringDTO>? URangoOferta { get; set; }
+        public List<StringDTO>? URangoTasas { get; set; }
+        public List<StringDTO>? UTipoCliente { get; set; }
         public List<StringDTO>? UUsuario { get; set; }
         public List<StringDTO>? UTipoBase { get; set; }
     }
