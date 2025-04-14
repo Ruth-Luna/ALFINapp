7fc421e2b71a63cd17ef3ff8ddb723dde285c8f3
santiagoVL03 <santiagovl0308@gmail.com>
2025-04-14 08:29:48 -0500

    feat: Add pagination functionality to Leads management; implement GestionPag action in LeadsController, update Gestion view for pagination controls, and create Gestion.js for page handling

--- Controllers/LeadsController.cs
+++ Controllers/LeadsController.cs
@@ -47,6 +47,7 @@ namespace ALFINapp.Controllers
                 ViewData["ClientesTipificados"] = dataInicio.clientesTipificados;
                 ViewData["UsuarioNombre"] = dataInicio.Vendedor != null ? dataInicio.Vendedor.NombresCompletos : "Usuario No Encontrado";
                 ViewData["ClientesTraidosDBALFIN"] = dataInicio.ClientesAlfin;
+                ViewData["PaginaActual"] = paginaInicio;
                 return View("Gestion", dataInicio.ClientesA365);
             }
             else if (rol == 2)
@@ -61,6 +62,53 @@ namespace ALFINapp.Controllers
             }
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GestionPag(int paginaInicio = 0, int paginaFinal = 1)
+        {
+            int? usuarioId = HttpContext.Session.GetInt32("UsuarioId");
+            if (usuarioId == null)
+            {
+                TempData["MessageError"] = "Ha ocurrido un error en la autenticación";
+                return RedirectToAction("Index", "Home");
+            }
+            int? rol = HttpContext.Session.GetInt32("RolUser") ?? 0;
+            if (rol == 0)
+            {
+                TempData["MessageError"] = "No se ha encontrado el rol del usuario";
+                return RedirectToAction("Index", "Home");
+            }
+
+            var executeInicio = await _useCaseGetAsignacionLeads.Execute(usuarioId.Value, rol.Value, paginaInicio, paginaFinal);
+            if (!executeInicio.IsSuccess || executeInicio.Data == null)
+            {
+                TempData["MessageError"] = executeInicio.Message;
+                return RedirectToAction("Redireccionar", "Error");
+            }
+
+            if (rol.Value == 3)
+            {
+                var dataInicio = executeInicio.Data;
+                ViewData["TotalClientes"] = dataInicio.clientesTotal;
+                ViewData["ClientesPendientes"] = dataInicio.clientesPendientes;
+                ViewData["ClientesTipificados"] = dataInicio.clientesTipificados;
+                ViewData["UsuarioNombre"] = dataInicio.Vendedor != null ? dataInicio.Vendedor.NombresCompletos : "Usuario No Encontrado";
+                ViewData["ClientesTraidosDBALFIN"] = dataInicio.ClientesAlfin;
+                return Json (new
+                {
+                    data = dataInicio.ClientesA365
+                });
+            }
+            else if (rol == 2)
+            {
+                var dataInicio = executeInicio.Data;
+                return View("GestionS", dataInicio);
+            }
+            else
+            {
+                TempData["MessageError"] = "El rol del usuario no es válido";
+                return RedirectToAction("Redireccionar", "Error");
+            }
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ wwwroot/js/Leads/Gestion.js
@@ -0,0 +1,6 @@
+actualpage = 1;
+function actPagGestion(page) {
+    const baseUrl = window.location.origin;
+    const url = `${baseUrl}/Gestion/GestionPage/${page}`;
+        
+}
