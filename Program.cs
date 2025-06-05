8cec8f3f2239b5ce97a257a21758c0fac06f09d8
santiagoVL03 <santiagovl0308@gmail.com>
2025-06-05 15:32:32 -0500

    fix: Update connection string in appsettings.Development.json and refine .gitignore for test units

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
-using Microsoft.Extensions.FileProviders; // ← Agregar esta línea
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory()) 
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) 
-    .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true) // Agregar archivo local si existe
     .AddEnvironmentVariables();
 
 var defaultCulture = new CultureInfo("es-ES");
