21e8b71395f5405f22233ce793bd360d4e93ada4
santiagoVL03 <santiagovl0308@gmail.com>
2025-08-29 11:17:33 -0500

    feat: Enhance comment updating functionality to handle multiple phone numbers

--- Datos/DAO/Tipificaciones/DAO_ActualizarComentarios.cs
+++ Datos/DAO/Tipificaciones/DAO_ActualizarComentarios.cs
@@ -1,3 +1,4 @@
+using ALFINapp.Infrastructure.Persistence.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ALFINapp.Datos.DAO.Tipificaciones
@@ -34,6 +35,46 @@ namespace ALFINapp.Datos.DAO.Tipificaciones
                 }
                 else
                 {
+                    var cliente_telefono = await _context.clientes_enriquecidos
+                        .FirstOrDefaultAsync(ct => ct.Telefono1 == telefono);
+                    if (cliente_telefono != null)
+                    {
+                        cliente_telefono.ComentarioTelefono1 = comentario;
+                        await _context.SaveChangesAsync();
+                        return (true, "Comentario guardado correctamente.");
+                    }
+                    var cliente_telefono2 = await _context.clientes_enriquecidos
+                        .FirstOrDefaultAsync(ct => ct.Telefono2 == telefono);
+                    if (cliente_telefono2 != null)
+                    {
+                        cliente_telefono2.ComentarioTelefono2 = comentario;
+                        await _context.SaveChangesAsync();
+                        return (true, "Comentario guardado correctamente.");
+                    }
+                    var cliente_telefono3 = await _context.clientes_enriquecidos
+                        .FirstOrDefaultAsync(ct => ct.Telefono3 == telefono);
+                    if (cliente_telefono3 != null)
+                    {
+                        cliente_telefono3.ComentarioTelefono3 = comentario;
+                        await _context.SaveChangesAsync();
+                        return (true, "Comentario guardado correctamente.");
+                    }
+                    var cliente_telefono4 = await _context.clientes_enriquecidos
+                        .FirstOrDefaultAsync(ct => ct.Telefono4 == telefono);
+                    if (cliente_telefono4 != null)
+                    {
+                        cliente_telefono4.ComentarioTelefono4 = comentario;
+                        await _context.SaveChangesAsync();
+                        return (true, "Comentario guardado correctamente.");
+                    }
+                    var cliente_telefono5 = await _context.clientes_enriquecidos
+                        .FirstOrDefaultAsync(ct => ct.Telefono5 == telefono);
+                    if (cliente_telefono5 != null)
+                    {
+                        cliente_telefono5.ComentarioTelefono5 = comentario;
+                        await _context.SaveChangesAsync();
+                        return (true, "Comentario guardado correctamente.");
+                    }
                     return (false, "Número no encontrado.");
                 }
             }
